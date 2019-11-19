52b4592a61249fb5bbb01c397d1a236371b6bf78
Nikita Buzanov <github@buzanovn.ru>
2019-11-19 10:34:45 +0300

    Update predeploy.sh

--- predeploy.sh
+++ predeploy.sh
@@ -1,11 +1,49 @@
-#!/bin/bash
+#!/bin/sh
+# The intent of this script is to prepare environment for deploying any thing to a server
+# We check the 
 
-[[ -z "$1" ]] && SSH_DEPLOY_KEY=${DEPLOY_KEY} || SSH_DEPLOY_KEY="$1"
 
-which ssh-agent || ( apt-get update -y && apt-get install openssh-client -y )
-mkdir -p ~/.ssh
-eval $(ssh-agent -s)
-if [[ -f /.dockerenv ]]; then
-  echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
+if [[ -z "$1" ]]; then
+    if [[ -z "${DEPLOY_KEY}" ]]; then
+        echo "No deploy key found, nothing to add" 1>&2
+        exit 2
+    else
+        SSH_DEPLOY_KEY="${DEPLOY_KEY}"
+        echo "Found deploy key"
+    fi
+else
+    SSH_DEPLOY_KEY="${DEPLOY_KEY}"
+    echo "Found deploy key"
 fi
-ssh-add <(echo "$SSH_DEPLOY_KEY")
+
+function preinstall_ubuntu {
+    apt-get update -y && apt-get install openssh-client -y
+}
+
+function preinstall_alpine {
+    apk update && apk add openssh bash
+}
+
+DISTRO_ID=$(cat /etc/os-release | grep ID | head -n1 | cut -d= -f2)
+
+case ${DISTRO_ID}
+    ubuntu)
+    elementary)
+    mint)
+        preinstall_ubuntu
+        ;;
+    alpine)
+        preinstall_alpine
+        ;;
+    *)
+        echo "Unknow distribution id: $DISTRO_ID" 1>&2
+        exit 3
+        ;;
+esac
+
+mkdir -p ~/.ssh && eval $(ssh-agent -s)
+if [[ -f /.dockerenv ]]; then
+  printf "Host *\n\tStrictHostKeyChecking no\n\n" | tee $HOME/.ssh/config
+fi
+
+bash -c "ssh-add <(echo ${SSH_DEPLOY_KEY}) && echo 'Ready to deploy'"
