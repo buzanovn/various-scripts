5c5cae19e160697adfee1dcdbc87c3240a329226
Nikita Buzanov <github@buzanovn.ru>
2019-11-19 18:54:09 +0300

    Update predeploy.sh

--- predeploy.sh
+++ predeploy.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # The intent of this script is to prepare environment for deploying anything to a server 
 
 if [ -z "$1" ]; then
@@ -14,24 +14,9 @@ else
     echo "Found deploy key"
 fi
 
-DISTRO_ID=$(cat /etc/os-release | grep ID | head -n1 | cut -d= -f2)
-
-case ${DISTRO_ID} in
-    ubuntu)
-            apt-get update -qq && apt-get install openssh-client -y
-            ;;
-    alpine)
-            apk update && apk add openssh
-            ;;
-    *)
-            echo "Unknow distribution id: $DISTRO_ID" 1>&2
-            exit 3
-            ;;
-esac
-
 mkdir -p ~/.ssh && eval $(ssh-agent -s)
 if [ -f /.dockerenv ]; then
   printf "Host *\n\tStrictHostKeyChecking no\n\n" > $HOME/.ssh/config
 fi
 
-bash -c 'ssh-add <(echo "${SSH_DEPLOY_KEY}")' && echo 'Ready to deploy'
+ssh-add <(echo "${SSH_DEPLOY_KEY}")
