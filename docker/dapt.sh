6be031baaecbc438080bef8f4ad33fe9b6eb9f92
Nikita Buzanov <github@buzanovn.ru>
2020-02-02 17:16:53 +0300

    Create dapt.sh

---
+++ docker/dapt.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+function apt_update() {
+    apt-get update -qq
+}
+
+function apt_install() {
+    apt-get install -qq --no-install-recommends $@
+}
+
+ARG="$1"
+shift
+
+case ${ARG} in
+apt-update)
+    apt_update
+    ;;
+apt-install)
+    apt_update && apt_install $@
+    ;;
+apt-install-from-file)
+    apt_update && apt_install $(cat $2 | egrep -v "^\s*(#|$)")
+    ;;
+apt-clean)
+    apt-get clean && rm -rf /var/lib/apt/{lists,cache}
+    ;;
+set-noninteractive)
+    echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
+    ;;
+esac
