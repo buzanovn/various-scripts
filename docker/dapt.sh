1a7f4845964e1826c5cb1d3d8ea8af1d4f48c279
Nikita Buzanov <github@buzanovn.ru>
2020-05-03 20:46:58 +0300

    Update dapt.sh

--- docker/dapt.sh
+++ docker/dapt.sh
@@ -1,66 +1,60 @@
 #!/bin/sh
 
-PIP3=$(command -v pip3)
-
-pip_compile() {
-    CFLAGS="-g0 -Wl,--strip-all -I/usr/include:/usr/local/include -L/usr/lib:/usr/local/lib" \
-    $PIP3 install -q \
-      --compile \
-      --no-cache-dir \
-      --global-option=build_ext \
-      --global-option="-j$(expr $(nproc) - 1)" \
-    $@
+apt_update() {
+    echo "Updating"
+    apt-get update -yqq
 }
 
-pip_install() {
-    $PIP3 install -q --no-cache-dir $@
+apt_install() {
+    apt-get install -yqq --no-install-recommends $@
 }
 
-do_from_file() {
-    if [ ! -e $2 ]; then
-        echo "File $2 does not exist"
-    elif [ ! -s $2 ]; then
-        echo "File $2 is empty, nothing to install"
+apt_install_from_file() {
+    local package_list="$(cat "$1" | egrep -v "^\s(#|$)")"
+    if [ -z "$package_list" ]; then
+        echo "File $1 is empty, nothing to install"
     else
-        $($1 -r $2)
+        apt_install $package_list
     fi
 }
 
-do_from_directory() {
-    local file_list="$(ls $2 | sort -t - -k 1 -g)"
-    if [ -z "$file_list" ]; then
+apt_install_from_directory() {
+    local file_list="$(ls $1 | sort -t - -k 1 -g)"
+    if [ -z "$file_list" ]; then 
         echo "Directory $1 is empty, nothing to install"
     else
-        for f in $file_list; do do_from_file $1 $2/$f; done
+        for f in $file_list; do 
+            apt_install_from_file "$1/$f"
+        done
     fi
 }
 
-ARG="$1"
+ACTION="$1"
 shift
 
-case ${ARG} in
-compile)
-    pip_compile $@
+case ${ACTION} in
+update)
+    apt_update
     ;;
 install)
-    pip_install $@
-    ;;
-compile-from-file)
-    do_from_file "pip_compile" $1
+    apt_update && apt_install $@
     ;;
 install-from-file)
-    do_from_file "pip_install" $1
-    ;;
-compile-from-directory)
-    do_from_directory "pip_compile" $1
+    apt_update && apt_install_from_file $1
     ;;
 install-from-directory)
-    do_from_directory "pip_install" $1
+    apt_update && apt_install_from_directory $1
     ;;
-self-upgrade)
-    $PIP3 install -q --upgrade pip && hash -r pip3
+clean)
+    apt-get clean && rm -rf /var/lib/apt/{lists,cache}
+    ;;
+set-noninteractive)
+    echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
+    echo '* libraries/restart-without-asking boolean true' | debconf-set-selections
+    echo 'Dpkg::Use-Pty "0";' > /etc/apt/apt.conf.d/00usepty
     ;;
 *)
-    $PIP3 $ARG $@
+    echo "Unknown action '${ACTION}'"
+    exit 1
     ;;
 esac
