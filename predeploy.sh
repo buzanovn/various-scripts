1e72751f62ade7c70ff8160d100d4dce2265d4fd
Nikita Buzanov <github@buzanovn.ru>
2020-05-03 14:45:54 +0300

    Update predeploy.sh

--- predeploy.sh
+++ predeploy.sh
@@ -1,23 +1,31 @@
-#!/bin/bash
-# The intent of this script is to prepare environment for deploying anything to a server 
-
-if [ -z "$1" ]; then
-    if [ -z "${DEPLOY_KEY}" ]; then
-        echo "No deploy key found, nothing to add" 1>&2
-        exit 2
-    else
-        SSH_DEPLOY_KEY="${DEPLOY_KEY}"
-        echo "Found deploy key"
-    fi
-else
-    SSH_DEPLOY_KEY="${DEPLOY_KEY}"
-    echo "Found deploy key"
+#!/bin/sh
+set -u
+if [ -f /etc/os-release ]; then
+  eval $(cat /etc/os-release)
+  [ -z "$ID" ] && exit 1
 fi
 
-mkdir -p ~/.ssh 
+case $ID in
+  alpine)
+    INSTALL_PACKAGES="apk update -q && apk add openssh-client -q"
+    ;;
+  ubuntu)
+    INSTALL_PACKAGES="apt-get update -yqq -o=Dpkg::Use-Pty=0 && apt-get install openssh-client -yqq -o=Dpkg::Use-Pty=0"
+    ;;
+  *)
+    echo "Unsupported distribution $ID"
+    exit 2
+    ;;
+esac
+
+which ssh-agent || eval "$INSTALL_PACKAGES"
+mkdir -p $HOME/.ssh
 eval $(ssh-agent -s)
-if [ -f /.dockerenv ]; then
-  printf "Host *\n\tStrictHostKeyChecking no\n\n" > $HOME/.ssh/config
-fi
+printf "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
 
-ssh-add <(echo "${SSH_DEPLOY_KEY}")
+echo "${DEPLOY_KEY}" | base64 -d | ssh-add - >/dev/null 2> /tmp/ssh-add-err.log
+EXIT_CODE=$?
+[ $EXIT_CODE -ne 0 ] && { echo "The key was not added"; cat /tmp/ssh-add-err.log } || echo "The key was successfully added"
+exit $EXIT_CODE
+
+
