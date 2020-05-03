51ac47722137c75c1fefca0203bccebbc43205dc
Nikita Buzanov <github@buzanovn.ru>
2020-05-03 15:43:14 +0300

    Update dpip.sh

--- docker/dpip.sh
+++ docker/dpip.sh
@@ -1,12 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 PIP3=$(command -v pip3)
 
-ARG="$1"
-shift
-
-case ${ARG} in
-compile)
+pip_compile() {
     CFLAGS="-g0 -Wl,--strip-all -I/usr/include:/usr/local/include -L/usr/lib:/usr/local/lib" \
     $PIP3 install -q \
       --compile \
@@ -14,12 +10,49 @@ compile)
       --global-option=build_ext \
       --global-option="-j$(expr $(nproc) - 1)" \
     $@
+}
+
+pip_install() {
+     $PIP3 install -q --no-cache-dir $@
+}
+
+do_from_file() {
+    $($1 -r $2)
+}
+
+do_from_directory() {
+    local file_list="$(ls $1 | sort -t - -k 1 -g)"
+    if [ -z "$file_list" ];
+        echo "Directory $1 is empty, nothing to install"
+    else
+        for f in $file_list; do do_from_file $1 $2/$f; done
+    done
+}
+
+ARG="$1"
+shift
+
+case ${ARG} in
+compile)
+    pip_compile $@
+    ;;
+install)
+    pip_install $@
+    ;;
+compile-from-file)
+    do_from_file "pip_compile" $1
+    ;;
+install-from-file)
+    do_from_file "pip_install" $1
+    ;;
+compile-from-directory)
+    do_from_directory "pip_compile" $1
+    ;;
+install-from-directory)
+    do_from_directory "pip_install" $1
     ;;
 self-upgrade)
     $PIP3 install -q --upgrade pip && hash -r pip3
-    ;;
-install-package)
-    $PIP3 install -q --no-cache-dir $@
     ;;
 *)
     $PIP3 $ARG $@
