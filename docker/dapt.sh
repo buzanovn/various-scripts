a2b83a4b7384e4fc589268fcff211798a16a5195
Nikita Buzanov <github@buzanovn.ru>
2020-05-03 14:54:57 +0300

    Update dapt.sh

--- docker/dapt.sh
+++ docker/dapt.sh
@@ -1,11 +1,22 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 function apt_update() {
-    apt-get update -qq
+    echo "Updating"
+    apt-get update -yqq
 }
 
 function apt_install() {
-    apt-get install -qq --no-install-recommends $@
+    apt-get install -yqq --no-install-recommends $@
+}
+
+function apt_install_from_file() {
+    apt_install $(cat $1 | egrep -v "^\s*(#|$)")
+}
+
+function apt_install_from_directory() {
+    for f in $(ls $1 | sort -t - -k 1 -g); do 
+        apt_install_from_file $f
+    done
 }
 
 ARG="$1"
@@ -21,10 +32,15 @@ install)
 install-from-file)
     apt_update && apt_install $(cat $1 | egrep -v "^\s*(#|$)")
     ;;
+install-from-directory)
+    apt_update && { for f in $(ls $1 | sort -t - -k 1 -g); do apt_install $(cat $f | egrep -v "^\s*(#|$)"); done }
+    ;;
 clean)
     apt-get clean && rm -rf /var/lib/apt/{lists,cache}
     ;;
 set-noninteractive)
     echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
+    echo '* libraries/restart-without-asking boolean true' | debconf-set-selections
+    echo 'Dpkg::Use-Pty "0";' > /etc/apt/apt.conf.d/00usepty
     ;;
 esac
